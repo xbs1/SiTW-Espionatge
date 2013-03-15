d85e355e77d35d939821d5407d784ada2203c525
user05 <user05@alcatel02.(none)>
2013-03-15 11:01:04 +0100

    Last mod

--- app/views.py
+++ app/views.py
@@ -1,6 +1,19 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/home')
 def home():
-	return "Nothing here yet!!!"
+	user = { 'name': 'Barcenas', 'city' : 'Madrid' } # Mockup
+	sobres = [ # fake array of posts
+		{
+			'company': { 'name': 'ACS' },
+			'Amount': '100.000EUR'
+		},
+		{
+			'company': { 'name': 'FCC' },
+			'Amount': '150.000EUR'
+		},
+	]
+	return render_template("index.html",pagetitle = 'Home',user = user, envelopes=sobres)
+
